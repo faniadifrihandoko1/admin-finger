b06f7adccd92ebf90f7b16ba1f05a5c6b65779c3
faniadifrihandoko1 <faniadifrihandoko01@gmail.com>
2025-09-10 11:16:05 +0700

    feat(fingerprint) : integration delete

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import React from "react";
 import { DashboardView } from "@/app/view/(dashboard)/dashboard";
 
 export default function DashboardPage() {
--- app/(dashboard)/fingerprints/page.tsx
+++ app/(dashboard)/fingerprints/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import React from "react";
 import { FingerprintView } from "@/app/view/(dashboard)/fingerprint";
 
 export default function FingerprintPage() {
